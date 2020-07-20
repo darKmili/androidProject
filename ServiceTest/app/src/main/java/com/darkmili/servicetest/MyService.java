f3d47b5d41de1267eccde35b104bc1050134f103
hong <1227642494@qq.com>
2020-07-20 17:59:40 +0800

    前台Service代码

--- ServiceTest/app/src/main/java/com/darkmili/servicetest/MyService.java
+++ ServiceTest/app/src/main/java/com/darkmili/servicetest/MyService.java
@@ -1,12 +1,17 @@
 package com.darkmili.servicetest;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
     private static final String TAG="MyService";
@@ -37,6 +42,14 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.v(TAG,"OnCreate");
+
+
+        Intent intent=new Intent(this,MainActivity.class);
+        PendingIntent activity = PendingIntent.getActivity(this, 0, intent, 0);
+        Notification notification=new NotificationCompat.Builder(this,"My_Service").setAutoCancel(true).setContentTitle("title of notify")
+                .setContentText("你好呀").setSmallIcon(R.drawable.ic_launcher_background).setContentIntent(activity).build();
+
+        startForeground(1,notification);
 
     }
 
