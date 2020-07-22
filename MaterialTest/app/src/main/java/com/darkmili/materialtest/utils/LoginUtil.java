961c27fb1794f1132000e65dd08be32e9aba34c4
hong <1227642494@qq.com>
2020-07-22 18:02:39 +0800

    持久化保存账号密码的实现，待完成

--- MaterialTest/app/src/main/java/com/darkmili/materialtest/utils/LoginUtil.java
+++ MaterialTest/app/src/main/java/com/darkmili/materialtest/utils/LoginUtil.java
@@ -1,8 +1,23 @@
 package com.darkmili.materialtest.utils;
 
 
+import android.content.Context;
+
+import com.darkmili.materialtest.entity.MyApplication;
 import com.darkmili.materialtest.entity.User;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,13 +28,7 @@ public class LoginUtil {
     private static int count;
     private static Map<Integer, User> userMap = new HashMap<Integer, User>();
 
-    static {
-        userMap.put(Integer.parseInt("123"), new User("123", "123"));
-        userMap.put(Integer.parseInt("456"), new User("456", "123"));
-        userMap.put(Integer.parseInt("567"), new User("567", "123"));
-        userMap.put(Integer.parseInt("789"), new User("789", "123"));
-        count = 4;
-    }
+
 
     public static boolean login_pass(String name, String pass) {
         if (name == null || pass == null)
@@ -40,6 +49,16 @@ public class LoginUtil {
         //TODO 注册账号和密码，将账号密码存到hash表中
         count++;
         userMap.put(Integer.parseInt(name), new User(name, pass));
+        //TODO 持久化保存密码
+        try {
+            JSONObject jsonObject=new JSONObject();
+//            jsonObject.put(new User(name,pass));
+            FileOutputStream outputStream = MyApplication.getContext().openFileOutput("user", Context.MODE_APPEND);
+            BufferedWriter writer=new BufferedWriter(new OutputStreamWriter(outputStream));
 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+
 }
